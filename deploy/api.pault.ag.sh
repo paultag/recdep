dd84a833a681b7242fd5c1d012342a37b869d989
Paul Tagliamonte <tag@pault.ag>
2014-07-13 23:53:09 -0400

    Fiddle layout.

---
+++ deploy/api.pault.ag.sh
@@ -0,0 +1,11 @@
+# Config for the API
+# Change each of the entries below.
+
+SECRET_KEY=
+DATABASE_URL=postgis://recdep:password@postgres.prod.lucifer.pault.ag:5432/recdep
+
+TWILIO_ACCOUNT_SID="sOMeHasHofNumbersAndLETTers"
+TWILIO_AUTH_TOKEN="moreHashEsHere"
+
+TWILIO_TO_NUMBER="5558675309"
+TWILIO_FROM_NUMBER="8005553333"
